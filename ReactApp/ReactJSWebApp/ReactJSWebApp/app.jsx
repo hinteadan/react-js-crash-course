76ec763de3a7c2371125010d41aaeaa4dd7dd1f5
Hintee <dan.hintea@recognos.ro>
2017-08-30 13:16:10 +0300

    Separate file for JSX

---
+++ ReactApp/ReactJSWebApp/ReactJSWebApp/app.jsx
@@ -0,0 +1,9 @@
+﻿class App extends React.Component{
+    render(){
+        return (
+            <div>Hello from React Side from separate file</div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
