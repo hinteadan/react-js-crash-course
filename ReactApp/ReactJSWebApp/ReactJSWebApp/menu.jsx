8aafc30da81696e8f44a9af6980b98b65af24113
Hintee <dan.hintea@recognos.ro>
2017-08-31 10:10:37 +0300

    React sample

---
+++ ReactApp/ReactJSWebApp/ReactJSWebApp/menu.jsx
@@ -0,0 +1,41 @@
+﻿class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date(), name: '' };
+    }
+
+    myProcessor() {
+        this.setState({ date: new Date() });
+        //alert(this.state.date);
+    }
+
+    inputChanged(event) {
+        this.setState({ name: event.target.value });
+    }
+
+    render() {
+        return <div>Main Menu {this.state.date.toLocaleTimeString()}
+            Name: {this.state.name}
+            <Input change={this.inputChanged.bind(this)}/>
+            <Button clickHandler={this.myProcessor.bind(this)}></Button>
+                </div>;
+    }
+}
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <button onClick={this.props.clickHandler}>Button</button>;
+    }
+}
+
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <input type="text" onChange={this.props.change} />
+    }
+}
---
+++ ReactApp/ReactJSWebApp/ReactJSWebApp/wireup.jsx
@@ -0,0 +1,7 @@
+﻿class App extends React.Component {
+    render() {
+        return <div><Menu /></div>;
+    }
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('myDiv'));
